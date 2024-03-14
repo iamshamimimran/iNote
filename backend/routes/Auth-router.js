30d29f6a651838e726757f0d802f979ad57b4cbe
Shamim Imran <shamimimran35@gmail.com>
2024-03-14 16:17:13 +0530

    authToken Added and hash password added

--- backend/routes/Auth-router.js
+++ backend/routes/Auth-router.js
@@ -1,22 +1,51 @@
 import { Router } from "express";
 import { User } from "../models/User.js";
 import { body, validationResult } from 'express-validator';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+const JWT_SECRET = 'iAmn00b';
 const router = Router();
 
-
-router.post('/',[
+router.post('/createUser', [
   body('name').isLength({ min: 3 }),
   body('email').isEmail(),
   body('password').isLength({ min: 5 }),
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "Sorry, the user already exists" });
     }
-    res.send(req.body);
-    const user = User (req.body);
-    user.save()
-  });
 
+    const salt= await bcrypt.genSalt(10);
 
-  export default router;
+    const secPass =await bcrypt.hash(req.body.password, salt);
+
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email, 
+      password: secPass,
+    });
+
+    const data = {
+      user: {
+        id: user.id
+      }
+    };
+    
+    const authToken = jwt.sign(data, JWT_SECRET);
+    res.json({ authToken });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+export default router;
