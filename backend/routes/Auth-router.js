d7f22f60452122751de0ffd00263584976408649
Shamim Imran <shamimimran35@gmail.com>
2024-03-16 13:32:44 +0530

    update note api added

--- backend/routes/Auth-router.js
+++ backend/routes/Auth-router.js
@@ -137,5 +137,35 @@ router.post('/addnote', fetchUser, [
   }
 });
 
+//ROUTE : 3 --> Update notes all Notes
+
+router.put('/updatenote/:id', fetchUser, async (req, res) => {
+  try {
+    const { title, description, tag } = req.body;
+
+    const newNote = {};
+    if (title) newNote.title = title;
+    if (description) newNote.description = description;
+    if (tag) newNote.tag = tag;
+
+    let note = await Notes.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+
+    res.status(200).json({ success: true, note });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
 
 export default router;
