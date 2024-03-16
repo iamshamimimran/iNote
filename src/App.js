28107461f6e815669165e74850a527aa194bb418
Shamim Imran <shamimimran35@gmail.com>
2024-03-16 23:11:42 +0530

    Bug Fix

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-<Router>
-<Navbar/>
+      <Router>
+        <Navbar />
         <Routes>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
-</Router>
-
+      </Router>
     </div>
   );
 }
